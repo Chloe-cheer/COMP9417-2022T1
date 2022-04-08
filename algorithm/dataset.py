602ef7b68ff276297c75ebfc2dd39a7ac4e3245e
Chloe-cheer <chenyychloe@gmail.com>
2022-04-09 02:04:52 +1000

    add to do

--- algorithm/dataset.py
+++ algorithm/dataset.py
@@ -1,1 +1,18 @@
 """Utilities for loading and preprocessing the given dataset."""
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import normalize
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+from torch.utils.data import Dataset
+
+class DatasetLoader(Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+    def pre_processing(self):
+        pass
