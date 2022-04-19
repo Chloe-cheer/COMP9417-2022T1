e54b3daa8d1fb3652eec605d249dcd8db4aebea7
blakeim2000 <41655447+blakeim2000@users.noreply.github.com>
2022-04-19 23:25:56 +1000

    Update dataset.py

--- algorithm/dataset.py
+++ algorithm/dataset.py
@@ -1,70 +1,76 @@
 """Utilities for loading and preprocessing the given dataset."""
 import numpy as np
-import gc 
-from torch.utils.data import DataLoader
+import pandas as pd
+from matplotlib import pyplot as plt
 import mahotas as mt
+import gc
+import skimage.measure
+from torch.utils.data import DataLoader, random_split
+import torch
 
 def get_dataloaders():
-  # change path if needed
   y_train = np.load('y_train.npy')
   X_train = np.load('X_train.npy', mmap_mode='r')
-
-  # split the data in 60-20-20 
-  n_train = 514
+ 
+  n_train = 686
   n_validate = 172
 
-  train_data = []
-  validate_data = []
-  test_data = []
+  full_data = [] 
   i = 0 
 
-  ## First 514 rows are our "train data"
+  ## First 686 rows are our "train data"
   print("Preparing training data...")
   while i < n_train:
-      train_data.append([np.array(X_train[i]),y_train[i]])
+      full_data.append([np.array(X_train[i]),y_train[i]])
       i = i + 1
 
   print("Training data finished.")
   print("Preparing validation data...")
-  ## Next 172 are "test data"
-  while i < n_train + n_validate:
-      validate_data.append([np.array(X_train[i]),y_train[i]])
+  ## Last 172 are "validation data"
+  while i < len(X_train):
+      full_data.append([np.array(X_train[i]),y_train[i]])
       i = i + 1
 
   print("Validation data finished.")
-  print("Preparing test data...")
-  ## Last 172 are "test data"
-  while i < len(X_train):
-      test_data.append([np.array(X_train[i]),y_train[i]])
-      i = i + 1
 
-  print("Test data finished.")
   print("Packaging data into DataLoaders...")
-  train_dataloader = DataLoader( train_data, batch_size = 20)
-  validate_dataloader = DataLoader( validate_data, batch_size = 20)
-  test_dataloader = DataLoader( test_data, batch_size = 20)
+ 
+  torch.manual_seed(10)
+  train_data, validate_data = random_split(full_data, [686, 172])
+  train_dataloader = DataLoader(train_data, shuffle=True)
+  validate_dataloader = DataLoader(validate_data, shuffle=True)
+
 
   print("Freeing up memory...")
+  del full_data
   del train_data
   del validate_data
-  del test_data
   gc.collect()
   
-  return train_dataloader, validate_dataloader, test_dataloader 
+  return train_dataloader, validate_dataloader
 
-def get_haralick(input_dataloader):
+def extract_features(image):
+    # calculate haralick texture features for 4 types of adjacency
+    textures = mt.features.haralick(image)
+
+    # take the mean of it and return it
+    ht_mean = textures.mean(axis=0)
+    return ht_mean
+  
+def get_features(input_dataloader):
     haralick_features = []
-    i = 0 
+    pooling_features = []
+    y = []
     for batch_features, batch_label in iter(input_dataloader):
-        for batch_feature in batch_features:
-            gray =  np.dot(batch_feature[...,:3], [76.24499999999999, 149.685, 29.07]) #combines unormalisation and grey-scale conversion
+        j = 0 
+        while j < len(batch_features):
+            gray =  np.dot(batch_features[j][...,:3], [76.245, 149.685, 29.07]) #combines unormalisation and 
             haralick_feature = extract_features(gray.astype(int))
             haralick_features.append(haralick_feature)
-            i = i + 1 
-    return haralick_features
-
-train_dataloader, validate_dataloader, test_dataloader = get_dataloaders()
-# returns numpy arrays of haralick features
-haralick_train = get_haralick(train_dataloader)
-haralick_validate = get_haralick(validate_dataloader)
-haralick_test = get_haralick(test_dataloader)
+            gray_normalised =  np.dot(batch_features[j][...,:3], [0.299, 0.587, 0.114])
+            averaged = skimage.measure.block_reduce(gray_normalised, (64,64), np.average)
+            flat_array = averaged.flatten()
+            pooling_features.append(flat_array)
+            y.append(batch_label[j])
+            j = j + 1 
+    return haralick_features, pooling_features, y
